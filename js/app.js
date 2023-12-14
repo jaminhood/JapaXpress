4851f61c28af2d182c7af19a7064f7224570cc3f
Chikezie Ilodigwe <dikee5200@gmail.com>
2023-12-14 06:14:47 +0100

    The horizontal swap image for the flight search div was missing in the assets folder. I imported it from the figma file, title ion-swap-horizontal.png.

--- js/app.js
+++ js/app.js
@@ -19,17 +19,69 @@
 //   }
 // }
 
+// function validateLogin() {
+//   const email = document.querySelectorAll(".details input");
+//   const password = document.querySelectorAll(".details input");
+//   const error = document.getElementById("error-message");
+//   if (email === "Email" && password === "password") {
+//     return "Successful Login";
+//     error.style.color = "green";
+//     error.textContent = "Login successful";
+//   } else {
+//     return "Failed login";
+//     error.style.color = "red";
+//     error.textContent = "Invalid emal address or password. Please try again";
+//   }
+// }
+
+// function validateLogin() {
+//   // Get input values
+//   const email = document.querySelectorAll(".details input").value;
+//   const password = document.querySelectorAll(".details input").value;
+
+//   // Check if email and password are not empty
+//   if (email.trim() === "" || password.trim() === "") {
+//     // Display error message
+//     document.getElementById("display-Area").textContent = alert(
+//       "Email and password are required!"
+//     );
+//   } else {
+//     // Clear error message
+//     document.getElementById("error-message").textContent = "booking page";
+
+//     // You can add further logic here to handle the login process
+//     // For now, let's just log the inputs to the console
+//     return "Email: ", email;
+//     return "Password: ", password;
+//   }
+// }
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const LoginButton = document.querySelector(".login-btn");
+const display = document.getElementById("displayArea");
+const bookingPage = document.querySelector(".booking-page");
+const loginPage = document.querySelector(".logi-section");
+
 function validateLogin() {
-  const email = document.querySelectorAll(".details input");
-  const password = document.querySelectorAll(".details input");
-  const error = document.getElementById("error-message");
-  if (email === "Email" && password === "password") {
-    return "Successful Login";
-    error.style.color = "green";
-    error.textContent = "Login successful";
+  if (email === "" && password === "") {
+    display.style.display = this.prompt("Please enter email and/or password");
+  } else if (email === "Email" && password === "Password") {
+    bookingPage.classList.remove("hidden");
+    loginPage.classList.add("hidden");
   } else {
-    return "Failed login";
-    error.style.color = "red";
-    error.textContent = "Invalid emal address or password. Please try again";
+    // bookingPage.classList.remove("hidden");
+    // loginPage.classList.add("hidden");
+    display.style.display = this.prompt(
+      "Booking page is not ready! Okay bye!!!"
+    );
   }
 }
+
+// LoginButton = addEventListener("click", function () {
+//   if (email === "" || password === "") {
+//     this.alert("Please enter email and/or password");
+//   } else {
+//     // display.style.display = this.prompt("Booking page is not ready!");
+//   }
+// });
